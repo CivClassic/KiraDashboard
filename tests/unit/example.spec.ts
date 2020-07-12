dbd311b86c2247145164f1e2e90ac813781c6848
AngrySoundTech <angrysoundtech@gmail.com>
2020-07-11 22:00:16 -0400

    Zero out tests and remove useless comments

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,8 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import App from '@/App.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+describe('Example', () => {
+  it('is true', () => {
+    expect(true).toBe(true);
   });
 });
